bf11ecaf8ab9ea923a3a2062c60f8b685e273818
Benjamin Deneux <benjamindeneux@hotmail.com>
2021-10-03 12:03:00 +0200

    Fonction en php

---
+++ fonction.php
@@ -0,0 +1,11 @@
+<?php
+function writeSecretSentence ($parametre_1, $parametre_2)
+{
+  echo  "$parametre_1  s'incline face à  $parametre_2 ";
+    return;
+}
+
+writeSecretSentence( 'le chat','la lune' );
+writeSecretSentence( 'le chien', 'la pyramide');
+writeSecretSentence( 'le poulet', 'kfc');
+?>
